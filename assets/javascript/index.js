f8a8e23afae11ad5e78c4f0b92c26c52229bdd6b
GiacomoVisin <killerboss1150@gmail.com>
2026-03-19 23:37:58 +0100

    starting second exercise

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -48,6 +48,12 @@ for (let i=1; i < arrayBikes.length; i++){
     console.log(`la bici piu leggera è ${lighterBike.name} con peso di ${lighterBike.weight}`)
     
 
-    
+
+// Snack2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 
